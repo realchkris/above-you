a5bfbb3232aac81e5ac52101d75f66ee632d0947
realchkris <cinesich@gmail.com>
2025-03-14 15:34:10 +0100

    Fixed Tailwind classes for proper button image scaling

--- above-you-client/src/components/Header.vue
+++ above-you-client/src/components/Header.vue
@@ -4,11 +4,18 @@
 
 	<header class="header-container">
 
-		<img class="image-container" src="../assets/ay_logo.png">
-
+		<!-- Navbar Button -->
 		<button class="round-button bg-ay-lavender hover:bg-ay-purple">
-			<img class="icon-button" src="../assets/navbar.png">
+			<img class="icon-button icon-sm" src="../assets/navbar.png">
 		</button>
+
+		<!-- Above You Logo -->
+		<img class="image-sm" src="../assets/ay_logo.png">
+
+		<!-- Login Icon -->
+		<button class="round-button bg-ay-lavender hover:bg-ay-purple">
+      		<img class="icon-button icon-md" src="../assets/login.png">
+    	</button>
 
 	</header>
 
