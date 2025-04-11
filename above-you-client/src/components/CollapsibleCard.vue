d02db4408e40dcf9035beff6280fbc074b61fbaa
realchkris <cinesich@gmail.com>
2025-04-11 12:30:07 +0200

    fix(ui): reworked data column buttons

--- above-you-client/src/components/CollapsibleCard.vue
+++ above-you-client/src/components/CollapsibleCard.vue
@@ -1,11 +1,10 @@
 <template>
-	<div :class="['w-full', { 'flex-grow': !isOpen }]"> <!-- Apply flex-grow only when the card is closed -->
+	<div :class="['w-full', { 'flex-grow': !isOpen }]">
 
-		<!-- Apply h-full only when the card is closed -->
 		<div
-			class="base-container text-white flex items-center justify-between cursor-pointer"
-			:class="[bgColor, {'h-full': !isOpen}]"
-			@click="isOpen = !isOpen"
+		class="base-container text-white flex items-center justify-between cursor-pointer"
+		:class="[bgColor, {'h-full': !isOpen}]"
+		@click="isOpen = !isOpen"
 		>
 
 			<!-- Title -->
@@ -15,27 +14,32 @@
 			</div>
 
 			<!-- Button -->
-			<div class="text-sm text-gray-400">
-				<span v-if="isOpen">▲</span>
-				<span v-else>▼</span>
-			</div>
+			<transition name="chevron-fade" mode="out-in">
+				<component
+				:is="isOpen ? ChevronUp : ChevronDown"
+				class="w-4 h-4 text-gray-400"
+				:key="isOpen"
+				/>
+			</transition>
 
 		</div>
 
 		<transition name="fade">
 			<div
-				v-if="isOpen"
-				class="mt-4 w-full">
+			v-if="isOpen"
+			class="mt-4 w-full">
 				<slot />
 			</div>
 		</transition>
 
-	</div> <!-- End of the collapsible card container -->
+	</div>
 
 </template>
 
 <script setup>
+
 import { ref } from "vue";
+import { ChevronUp, ChevronDown } from "lucide-vue-next"
 
 const props = defineProps({
 	title: String,
@@ -51,4 +55,5 @@ const props = defineProps({
 });
 
 const isOpen = ref(props.initiallyOpen);
+
 </script>
